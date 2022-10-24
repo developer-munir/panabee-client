d10c00d1ad58ee5aad6d9e88ff591d44df1ff118
developer-munir <devolopermunir@gmail.com>
2022-10-24 22:26:27 +0600

    added responsive navbar

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,88 +1,98 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaPinterest } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-      <div>
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+  return (
+    <div>
+      <div className="navbar bg-[#5A20CB] text-[#CAD5E2]">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <li className="mb-2">
-                  <Link to='/'>Home</Link>
-                </li>
-                <li className="mb-2">
-                  <Link to='/courses'>Courses</Link>
-                </li>
-                <li className="mb-2">
-                  <Link to='/faq'>FAQ</Link>
-                </li>
-                <li className="mb-2">
-                  <Link to='/blog'>Blog</Link>
-                </li>
-                <li className="mb-2">
-                  <Link to='login'>Login</Link>
-                </li>
-                <li className="mb-2">
-                  <Link to='register'>Register</Link>
-                </li>
-              </ul>
-            </div>
-            <Link className="btn btn-ghost normal-case text-xl">
-              Panabee.io
-            </Link>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal p-0">
-              <li className="mr-2">
-                <Link to='/'>Home</Link>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#5A20CB] rounded-box w-52"
+            >
+              <li className="mb-2">
+                <Link to="/">Home</Link>
               </li>
-              <li className="mr-2">
-                <Link to='/courses'>Courses</Link>
+              <li className="mb-2">
+                <Link to="/courses">Courses</Link>
               </li>
-              <li className="mr-2">
-                <Link to='/faq'>FAQ</Link>
+              <li className="mb-2">
+                <Link to="/faq">FAQ</Link>
               </li>
-              <li className="mr-2">
-                <Link to='/blog'>Blog</Link>
+              <li className="mb-2">
+                <Link to="/blog">Blog</Link>
               </li>
-              <li className="mr-2">
-                <Link to='login'>Login</Link>
+              <li className="mb-2">
+                <Link to="login">Login</Link>
               </li>
-              <li className="mr-2">
-                <Link to='register'>Register</Link>
+              <li className="mb-2">
+                <Link to="register">Register</Link>
               </li>
             </ul>
           </div>
-          <div className="navbar-end">
-            <div className="form-control">
-              <label className="label cursor-pointer">
-                <input type="checkbox" className="toggle" checked />
-              </label>
-            </div>
+          <Link className="btn btn-ghost normal-case text-xl">
+            <span className="mr-2">
+              <FaPinterest></FaPinterest>
+            </span>
+            Panabee.io
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">
+            <li className="mr-2">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="mr-2">
+              <Link to="/courses">Courses</Link>
+            </li>
+            <li className="mr-2">
+              <Link to="/faq">FAQ</Link>
+            </li>
+            <li className="mr-2">
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li className="mr-2">
+              <Link to="login">Login</Link>
+            </li>
+            <li className="mr-2">
+              <Link to="register">Register</Link>
+            </li>
+            <li className="mr-2">
+              <Link>Dark Mode</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <span className="mr-3">Name</span>
+          <div className="dropdown dropdown-end">
+            <label  className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people"  alt=""/>
+              </div>
+            </label>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
