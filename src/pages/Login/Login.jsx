2d74fa8c39e767740262ddf93da62c8fe8a847ae
developer-munir <devolopermunir@gmail.com>
2022-10-25 01:33:16 +0600

    added register and login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React from "react";
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>login page</h1>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">LogIn now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <div className="md:w-1/2">
+                  <label className="flex justify-between items-center mt-2">
+                    <span className="border p-2 md:px-10 rounded-lg cursor-pointer">
+                      Forgot password?
+                    </span>
+                    ||
+                    <span className="border p-2 md:px-10 rounded-lg cursor-pointer">
+                      <Link to="/register">Create Account?</Link>
+                    </span>
+                  </label>
+                </div>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">LogIn</button>
+              </div>
+              <div className="md:w-1/2">
+                <div className="mt-3 flex justify-between items-center">
+                  <div className="form-control ">
+                    <button className="border  flex items-center p-2 md:px-10 rounded-lg cursor-pointer">
+                      <span>Sing In Google</span>
+                      <FaGoogle className="ml-2"></FaGoogle>
+                    </button>
+                  </div>
+                  <div className="form-control ">
+                    <button className="border flex items-center p-2 md:px-10 rounded-lg">
+                      <span>Sing In Github</span>{" "}
+                      <FaGithub className="ml-2"></FaGithub>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
-
+import React from "react";
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Register = () => {
-    return (
-        <div>
-            <h1>Register</h1>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full  shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="sure name"
+                  name="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo url</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="photo url"
+                  name="photo"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <Link to="/login" className="label-text-alt link link-hover">
+                    Already have an account?
+                  </Link>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+              <div className="md:w-1/2">
+                <div className="mt-3 flex justify-between items-center">
+                  <div className="form-control ">
+                    <button className="border flex items-center p-2 md:px-10 rounded-lg">
+                      <span>Sing In Google</span>
+                      <FaGoogle className="ml-2"></FaGoogle>
+                    </button>
+                  </div>
+                  <div className="form-control ">
+                    <button className="border flex items-center p-2 md:px-10 rounded-lg">
+                      <span>Sing In Github</span>
+                      <FaGithub className="ml-2"></FaGithub>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Register;
